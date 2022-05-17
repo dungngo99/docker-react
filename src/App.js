c636ac23ccac6e02a78c44534ce8db7296393a17
Dung Ngo <dungngotanlhp@gmail.com>
2022-05-17 13:26:38 -0500

    added a new feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Yes, save to reload. ahihi!
         </p>
         <a
           className="App-link"
